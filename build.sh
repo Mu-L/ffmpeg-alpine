b4c719f471bd36d8a42ab3461290cc998d51c538
Yveshield <yveshield@gmail.com>
2021-11-28 10:06:00 +0800

    correct build script

--- build.sh
+++ build.sh
@@ -3,19 +3,19 @@ cd dependencies
 docker build --rm -t yveshield/ffmpeg-alpine:4.4-dependencies .
 docker push yveshield/ffmpeg-alpine:4.4-dependencies
 cd ../build-stage
-docker build --rm -t yveshield/ffmpeg-alpine:4.2-buildstage .
-docker push yveshield/ffmpeg-alpine:4.2-buildstage
+docker build --rm -t yveshield/ffmpeg-alpine:4.4-buildstage .
+docker push yveshield/ffmpeg-alpine:4.4-buildstage
 cd ../runtime
-docker build --rm -t yveshield/ffmpeg-alpine:4.2-runtime .
-docker push yveshield/ffmpeg-alpine:4.2-runtime
+docker build --rm -t yveshield/ffmpeg-alpine:4.4-runtime .
+docker push yveshield/ffmpeg-alpine:4.4-runtime
 cd ../custom/alpine
-docker build --rm -t yveshield/ffmpeg-alpine:4.2 .
-docker push yveshield/ffmpeg-alpine:4.2
+docker build --rm -t yveshield/ffmpeg-alpine:4.4 .
+docker push yveshield/ffmpeg-alpine:4.4
 cd ../golang
-docker build --rm -t yveshield/ffmpeg-alpine:4.2-golang .
-docker push yveshield/ffmpeg-alpine:4.2-golang
-docker build --rm -t yveshield/ffmpeg-alpine:4.2-golang-1.15 .
-docker push yveshield/ffmpeg-alpine:4.2-golang-1.15
+docker build --rm -t yveshield/ffmpeg-alpine:4.4-golang .
+docker push yveshield/ffmpeg-alpine:4.4-golang
+docker build --rm -t yveshield/ffmpeg-alpine:4.4-golang-1.15 .
+docker push yveshield/ffmpeg-alpine:4.4-golang-1.15
 cd ../../example/ffmepg-cli
 docker build --rm -t yveshield/ffmpeg-alpine:example .
 docker push yveshield/ffmpeg-alpine:example
